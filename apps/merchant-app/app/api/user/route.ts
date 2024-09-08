e65b159c46fa5733d1b2e3da677c4a4c996dfb50
Chinmay Ghayal <113549699+chinmayyyyyy@users.noreply.github.com>
2024-09-08 12:59:25 +0530

    Update route.ts
    Checking for potential errors

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,17 +1,16 @@
-import { getServerSession } from "next-auth"
-import { NextResponse } from "next/server";
-import { authOptions } from "../../../lib/auth";
+import { NextResponse } from "next/server"
+import { PrismaClient } from "@repo/db/client";
+
+const client = new PrismaClient();
 
 export const GET = async () => {
-    const session = await getServerSession(authOptions);
-    if (session?.user) {
-        return NextResponse.json({
-            user: session.user
-        })
-    }
+    await client.user.create({
+        data: {
+            email: "asd",
+            name: "adsads"
+        }
+    })
     return NextResponse.json({
-        message: "You are not logged in"
-    }, {
-        status: 403
+        message: "hi there"
     })
 }
